d91d3bdd3c53068aeee10d114be71e987cfe1033
SantosoHeri <antoshielied@gmail.com>
2025-07-24 00:42:59 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/personal-web/",
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 });
